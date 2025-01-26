10d17522f199ed3d7025b67b4c52b5434e6f2648
tejareddy8888 <saitejapottanigari@gmail.com>
2025-01-27 00:22:02 +0100

    Fixed email copying

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,12 +19,6 @@ export default defineNuxtConfig({
     }
   },
 
-  nitro: {
-    prerender: {
-      routes: ['/resume.pdf']
-    }
-  },
-
   runtimeConfig: {
     githubToken: '',
     public: {
